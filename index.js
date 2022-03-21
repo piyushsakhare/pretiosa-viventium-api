fe88f029a7ad396a1a2916283985c31f42aef6c1
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-21 23:30:28 +0530

    cors changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const cors = require("cors");
 const authRoute = require("./routes/auth")
 const destinationRoute = require("./routes/destinations")
 const userRoute = require("./routes/users")
@@ -21,11 +22,18 @@ mongoose.connect(process.env.MONGO_URL , {
     console.log(err)
 })
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/destinations", destinationRoute)
+app.use(cors(corsOptions))
 
 app.listen(port, () => {
     console.log(`Server is running`)
