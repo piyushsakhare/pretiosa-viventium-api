5fdd3b7b0ca3d568ee49051212ed7b160b806a5f
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-24 15:35:21 +0530

    api changes

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    res.header("Access-Control-Allow-Headers", "Content-Type")
     next()
 })
 
