a9bbf078f77d6526f6296f97ab2202cb7248728a
piyushsakhare <piyushsakhare713@gmail.com>
2022-03-29 22:05:36 +0530

    login error

--- routes/auth.js
+++ routes/auth.js
@@ -25,19 +25,23 @@ router.post("/signup", async (req,res) => {
 // Sign In
 
 router.post("/login", async (req,res) => {
-    const user = await User.findOne({email : req.body.email})
-    !user && res.status(401).json("Worng email or password")
+    try{
+        const user = await User.findOne({email : req.body.email})
+        !user && res.status(401).json("Worng email or password")
 
-    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
-    const originalPassword = bytes.toString(CryptoJS.enc.Utf8)
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8)
 
-    originalPassword !== req.body.password && res.status(401).json("Worng email or password")
+        originalPassword !== req.body.password && res.status(401).json("Worng email or password")
 
-    const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY, {expiresIn : "5d"})
+        const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY, {expiresIn : "5d"})
 
-    const {password, ...info} = user._doc
+        const {password, ...info} = user._doc
 
-    res.status(200).json({...info, accessToken})
+        res.status(200).json({...info, accessToken})
+    }catch(err){
+        res.status(400).json()
+    }
 })
 
 module.exports = router
